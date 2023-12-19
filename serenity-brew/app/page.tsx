87d4ff5810e408b8ae7fc35f32e04e0bccb57322
yardleyfra <francoisyardley@gmail.com>
2023-12-19 01:18:43 -0500

    alltogether

--- serenity-brew/app/page.tsx
+++ serenity-brew/app/page.tsx
@@ -1,6 +1,12 @@
 
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
+import Centerimage from '../components/centerimage';
+import Location from '../components/location';
+import Hours from '../components/Hours';
+
+
+
 
 
 
@@ -10,6 +16,10 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroSection />
+      <Centerimage />
+      <Location />
+      <Hours />
+
 
     </div>
   );
